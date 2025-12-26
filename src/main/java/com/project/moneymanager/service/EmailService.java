9febe2386e4b6c2b1a43dd2614cd317bbe9f4990
priyanshijindal001 <priyanshi@pintar.co>
2025-12-26 22:52:10 +0530

    sending brevo email through http instead of smtp

--- src/main/java/com/project/moneymanager/service/EmailService.java
+++ src/main/java/com/project/moneymanager/service/EmailService.java
@@ -1,27 +1,56 @@
 package com.project.moneymanager.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final JavaMailSender mailSender;
+    private static final String BREVO_URL =
+            "https://api.brevo.com/v3/smtp/email";
+    private final RestTemplate restTemplate;
+    @Value("${spring.mail.password}")
+    private String apiKey;
 
-    public void sendEmail(String to, String subject, String body) {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("priyanshijindal2123@gmail.com");
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(body);
-            mailSender.send(message);
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-
-        }
+    public void sendEmail(
+            String toEmail,
+            String subject,
+            String htmlContent) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("api-key", apiKey);
+        String body = String.format("""
+                                            {
+                                              "sender": {
+                                                "name": "%s",
+                                                "email": "%s"
+                                              },
+                                              "to": [
+                                                { "email": "%s" }
+                                              ],
+                                              "subject": "%s",
+                                              "htmlContent": "%s"
+                                            }
+                                            """,
+                                    "Money Manager",
+                                    "priyanshijindal2123@gmail.com",
+                                    toEmail,
+                                    subject,
+                                    htmlContent.replace("\"", "\\\"")
+        );
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        restTemplate.postForEntity(
+                BREVO_URL,
+                request,
+                String.class
+        );
     }
 }
